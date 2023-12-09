e5bba048ac00815e97e714f932606cfda520e675
Meriem <123774566+maryqu3en@users.noreply.github.com>
2023-12-09 10:38:23 +0100

    Update jacobi.py

--- jacobi.py
+++ jacobi.py
@@ -1,15 +1,22 @@
 '''                                 Program made by SOUBIH Meriem and ZELLAL Nourelhouda || L2 G8                                     '''
 
-import numpy as np
 
+# importing the NumPy library for numerical operations
+import numpy as np
 # ------------------------ display welcome and guide message ------------------------
 def welcomeAndGuide():
+
+    """Display welcome message and program guide."""
+
     print("\n\n\t\t\t\t<<-------- Welcome to our program -------->>\n")
     print("\n-> This program solves a linear system of equations using the Jacobi iterative method.")
     print("\n-> The program works with either knowing the number of iterations OR having the convergence error.")
 
 # ------------------------ fill matrix with input values ----------------------------
 def fillMatrix(size):
+
+    """Prompt the user to fill the matrix with coefficients."""
+
     matrix = []
     for y in range(size):
         row = []
@@ -27,11 +34,17 @@ def fillMatrix(size):
 
 # --------------------- fill array with equation constants --------------------------
 def fillArray(size):
+
+    """Prompt the user to fill the array with constants."""
+
     return [float(input(f"\tEnter value of Vector of constant B[{i}]: ")) for i in range(size)]
 
 
 # ---------------------------- display matrix ---------------------------------------
 def displayMatrix(matrix):
+
+    """Display the matrix."""
+
     for row in matrix:
         print("\t", end="")
         print("\t".join(f"{elem}" for elem in row))
@@ -39,6 +52,9 @@ def displayMatrix(matrix):
 
 # ---------------------------- display result ---------------------------------------
 def displayResult(arr):
+
+    """Display the result of the linear system."""
+
     print("\n\n\t\t\t\t<<---------- Analysis completed -------->>\n")
     print("The solutions of the provided linear system are: \n")
     print("\n".join(f"x{i}: {elem}" for i, elem in enumerate(arr)))
@@ -46,6 +62,20 @@ def displayResult(arr):
 
 # ----------------------------- JACOBI ALGORITHM ------------------------------------
 def Jacobi(A, b, tolerance, max_iterations = 10000):
+
+    """
+    Solve a linear system using the Jacobi iterative method.
+
+    Parameters:
+    - A: Coefficient matrix
+    - b: Vector of constants
+    - tolerance: Convergence error (if -1, the method uses a fixed number of iterations)
+    - max_iterations: Maximum number of iterations
+
+    Returns:
+    - The solution vector
+    """
+
     n = len(b)
     x = [0] * n
     x_prime = [0] * n
@@ -54,16 +84,20 @@ def Jacobi(A, b, tolerance, max_iterations = 10000):
     b = np.array(b)
 
     while iteration < max_iterations:
-        for i in range(n): #lines
+        for i in range(n): 
             summation = 0
-            for j in range(n): #columns
+            for j in range(n): 
                 if j != i:
                     summation += A[i][j] * x[j]
             x_prime[i] = (b[i] - summation) / A[i][i]
+
+        # checking for convergence if tolerance is specified
         if tolerance != -1:
             if np.linalg.norm(np.array(x_prime) - np.array(x)) < tolerance:
                 print(f"\nFound system solution after {iteration} iterations.")
                 return x_prime
+
+        # updating the solution vector
         x = x_prime.copy()
         iteration += 1
 
@@ -72,6 +106,9 @@ def Jacobi(A, b, tolerance, max_iterations = 10000):
 
 # ------------------- check if matrix is diagonally dominant ------------------------
 def isDiagonallyDominant(arr):
+
+    """Check if a matrix is diagonally dominant."""
+
     size = len(arr)
     for i in range(size):
         diagonal_value = abs(arr[i][i])
@@ -83,6 +120,20 @@ def isDiagonallyDominant(arr):
 
 #  ------------------ check if matrix can be diagonally dominant --------------------
 def diagonalDominance(arr):
+
+    """
+    Make a matrix diagonally dominant.
+
+    If the matrix is not initially diagonally dominant, attempt to swap rows
+    to achieve diagonally dominant properties.
+
+    Parameters:
+    - arr: The matrix
+
+    Returns:
+    - The modified matrix if successful, otherwise None
+    """
+
     if not isDiagonallyDominant(arr):
         size = len(arr)
         for i in range(size):
@@ -90,11 +141,12 @@ def diagonalDominance(arr):
             row_sum = sum(abs(arr[i][k]) for k in range(size) if k != i)
             pos = -1
             if diagonal_value < row_sum:
+                # finding a row to swap with to achieve diagonal dominance
                 for j in range(size):
                     if j != i and abs(arr[j][i]) >= sum(abs(arr[j][k]) for k in range(size) if k != i):
                         pos = j
                         break
-
+                # swapping rows if a suitable row is found
                 if pos >= 0 and i != pos:
                     arr[i], arr[pos] = arr[pos][:], arr[i][:]
                     print(f"\n\tSwapped rows {i} and {pos}")
